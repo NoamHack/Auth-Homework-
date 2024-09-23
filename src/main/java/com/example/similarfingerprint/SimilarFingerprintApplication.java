a28f7042c3731c52f5740b6e72ff983b9ce7d670
Noam Levy <noam8200@gmail.com>
2024-09-23 10:15:32 +0300

    Initial Commit

---
+++ src/main/java/com/example/similarfingerprint/SimilarFingerprintApplication.java
@@ -0,0 +1,13 @@
+package com.example.similarfingerprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarFingerprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimilarFingerprintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/similarfingerprint/SimilarFingerprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.similarfingerprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarFingerprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
